75e8e4c669b1546844c46a9d911f7f30e97c2f44
C-Assignments-And-projects <sameersharma8383@gmail.com>
2025-11-25 14:19:54 +0530

    4th Assignment of C++

--- Assign04.cpp
+++ Assign04.cpp
@@ -1,218 +1,237 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
-// Function Declarations
+// Functions Definition
 void swap(int &, int &);
 int add(int, int, int = 0);
-float areaOfCircle(float);
-int areaOfRectangle(int, int);
-float areaOfTriangle(int, int, int);
+float area(float);
+int area(int, int);
+float area(float, float, float);
 int max(int, int);
 double max(double, double);
-int Add(int, int);
-float Add(float, float);
-double Add(int, double);
-double Add(double, int);
-
-//-----------------------------------------------------------------
 // Q1. Swap Two Numbers (Call by Reference)
 void swap(int &a, int &b)
 {
-    a = (a + b) - (b = a);
+     b = (a + b) - (a = b);
 }
-void printSwap()
+void printSwapValues()
 {
-    cout << endl << "*************************************************************************" << endl;
-    cout << endl << " Q1. Define a C++ program to swap two numbers using call by reference." << endl;
-    cout << endl << "*************************************************************************" << endl << endl;
+     cout << endl
+          << "*************************************************************************" << endl;
+     cout << endl
+          << " Q1. Define a C++ program to swap two numbers using call by reference." << endl;
+     cout << endl
+          << "*************************************************************************" << endl
+          << endl;
 
-    int a, b;
-    cout << "Enter two numbers: ";
-    cin >> a >> b;
+     int a, b;
+     cout << "Enter Value for a: ";
+     cin >> a;
+     cout << "Enter Value for b: ";
+     cin >> b;
 
-    cout << "Before swapping: num1 = " << a << ", num2 = " << b << endl;
-    swap(a, b);
-    cout << "After swapping: num1 = " << a << ", num2 = " << b << endl;
+     cout << "Before swapping : a = " << a << ", b = " << b << endl;
+     swap(a, b);
+     cout << "After swapping  : a = " << a << ", b = " << b << endl;
 
-    cout << "\n*************************************************************************" << endl;
-    cout << "                       End of Program 1st.                               " << endl;
-    cout << "*************************************************************************\n" << endl;
+     cout << "\n*************************************************************************" << endl;
+     cout << "                       End of Program 1st.                               " << endl;
+     cout << "*************************************************************************\n"
+          << endl;
 }
-
-//-----------------------------------------------------------------
-// Q2. Sum of Two or Three Numbers using Default Argument
+// Q2. Sum of two or three numbers using default arguments.
 int add(int a, int b, int c)
 {
-    return a + b + c;
+     return a + b + c;
 }
-void printSumOf2or3Numbers()
+void printAdditionOf2or3Numbers()
 {
-    cout << endl << "*************************************************************************" << endl;
-    cout << endl << " Q2. Write a C++ program to calculate the sum of two or three numbers";
-    cout << " using function overloading." << endl;
-    cout << endl << "*************************************************************************" << endl << endl;
+     cout << endl
+          << "*************************************************************************" << endl;
+     cout << endl
+          << " Q2. Write a C++ function using the default argument that is able to add two or ";
+     cout << "  three numbers." << endl;
+     cout << endl
+          << "*************************************************************************" << endl
+          << endl;
 
-    int a, b, c;
-    char choice;
+     int a, b, c;
+     char choice;
 
-    cout << "Do you want to add two or three numbers? (Enter 2 or 3): ";
-    cin >> choice;
+     cout << "Enter your choice (2 for two numbers, 3 for three numbers): ";
+     cin >> choice;
 
-    if (choice == '2')
-    {
-        cout << "Enter two numbers: ";
-        cin >> a >> b;
-        int sum = add(a, b);
-        cout << "Sum of " << a << " and " << b << " is: " << sum << endl;
-    }
-    else if (choice == '3')
-    {
-        cout << "Enter three numbers: ";
-        cin >> a >> b >> c;
-        int s = add(a, b, c);
-        cout << "Sum of " << a << ", " << b << ", and " << c << " is: " << s << endl;
-    }
-    else
-    {
-        cout << "Invalid choice! Please enter 2 or 3." << endl;
-    }
-
-    cout << "\n*************************************************************************" << endl;
-    cout << "                       End of Program 2nd.                               " << endl;
-    cout << "*************************************************************************\n" << endl;
+     if (choice == '2')
+     {
+          // For two numbers
+          cout << "Enter two numbers: ";
+          cin >> a >> b;
+          int sum = add(a, b);
+          cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
+     }
+     else if (choice == '3')
+     {
+          // For three numbers
+          cout << "Enter three numbers: ";
+          cin >> a >> b >> c;
+          int s = add(a, b, c);
+          cout << "The sum of " << a << ", " << b << " and " << c << " is: " << s << endl;
+     }
+     else
+     {
+          cout << "Invalid choice! Please enter only 2 or 3." << endl;
+     }
+     cout << "\n*************************************************************************" << endl;
+     cout << "                       End of Program 2nd.                               " << endl;
+     cout << "*************************************************************************\n"
+          << endl;
 }
-
-//-----------------------------------------------------------------
-// Q3. Area Calculation using Function Overloading
-float areaOfCircle(float r)
+// Q3. Find area of Circle, Rectangle and Triangle using function overloading.
+// ------------------ Function Overloading -------------------
+float area(float r)
 {
-    return 3.14 * r * r;
+     return 3.14 * r * r; // Circle
 }
-int areaOfRectangle(int a, int b)
+
+int area(int a, int b)
 {
-    return a * b;
+     return a * b; // Rectangle
 }
-float areaOfTriangle(int a, int b, int c)
+
+float area(float a, float b, float c)
 {
-    float s = (a + b + c) / 2.0;
-    return sqrt(s * (s - a) * (s - b) * (s - c));
+     float s = (a + b + c) / 2.0;
+     return sqrt(s * (s - a) * (s - b) * (s - c)); // Triangle
 }
+
+// ------------------ Print Function -------------------
 void printAreaCalculations()
 {
-    cout << endl << "*************************************************************************" << endl;
-    cout << endl << " Q3. Write a C++ program to calculate the area of a circle, rectangle, and triangle using function overloading." << endl;
-    cout << endl << "*************************************************************************" << endl << endl;
+     cout << "\n*************************************************************************\n";
+     cout << " Q3. Calculate area of circle, rectangle & triangle using overloading.\n";
+     cout << "*************************************************************************\n\n";
 
-    float a, b, c;
+     float x, y, z;
 
-    cout << "Enter radius of the circle: ";
-    cin >> a;
-    float circleArea = areaOfCircle(a);
-    cout << "Area of Circle: " << circleArea << " cm^2" << endl;
+     // Circle
+     cout << "Enter radius of the circle: ";
+     cin >> x;
+     cout << "Area of Circle: " << area(x) << " cm^2" << endl;
 
-    cout << "Enter length and breadth of the rectangle: ";
-    cin >> a >> b;
-    int rectangleArea = areaOfRectangle(a, b);
-    cout << "Area of Rectangle: " << rectangleArea << " cm^2" << endl;
+     // Rectangle
+     cout << "Enter length and breadth of the rectangle: ";
+     cin >> x >> y;
+     cout << "Area of Rectangle: " << area((int)x, (int)y) << " cm^2" << endl;
 
-    cout << "Enter three sides of the triangle: ";
-    cin >> a >> b >> c;
-    float triangleArea = areaOfTriangle(a, b, c);
-    cout << "Area of Triangle: " << triangleArea << " cm^2" << endl;
+     // Triangle
+     cout << "Enter three sides of the triangle: ";
+     cin >> x >> y >> z;
+     cout << "Area of Triangle: " << area(x, y, z) << " cm^2" << endl;
 
-    cout << "\n*************************************************************************" << endl;
-    cout << "                       End of Program 3rd.                               " << endl;
-    cout << "*************************************************************************\n" << endl;
+     cout << "\n*************************************************************************\n";
+     cout << "                       End of Program 3rd.                               \n";
+     cout << "*************************************************************************\n";
 }
-
-//-----------------------------------------------------------------
-// Q4. Maximum of Two Numbers (Function Overloading)
+// Q4. Find maximum of two [both of the numbers is integer and real] numbers using Function Overloading
 int max(int a, int b)
 {
-    return (a > b) ? a : b;
+     return (a > b) ? a : b;
 }
+
 double max(double a, double b)
 {
-    return (a > b) ? a : b;
+     return (a > b) ? a : b;
 }
+
 void printMaxOf2Numbers()
 {
-    cout << endl << "**************************************************************************************" << endl;
-    cout << endl << " Q4. Write a C++ program to find the maximum of two numbers using function overloading." << endl;
-    cout << endl << "**************************************************************************************" << endl << endl;
+     cout << "\n**************************************************************************************\n";
+     cout << " Q4. Find the Maximum of Two Numbers using Function Overloading\n";
+     cout << "      (Both numbers can be Integer or Real)\n";
+     cout << "**************************************************************************************\n\n";
 
-    int a, b;
-    double x, y;
+     int a, b;
+     double x, y;
 
-    cout << "Enter two integers: ";
-    cin >> a >> b;
-    int maxInt = max(a, b);
-    cout << "Maximum of " << a << " and " << b << " is: " << maxInt << endl;
+     // Integer Input
+     cout << "Enter two integers: ";
+     cin >> a >> b;
+     int maxInt = max(a, b);
+     cout << "The maximum of " << a << " and " << b << " is: " << maxInt << endl;
 
-    cout << "\nEnter two floating-point numbers: ";
-    cin >> x >> y;
-    float maxFloat = max(x, y);
-    cout << "Maximum of " << x << " and " << y << " is: " << maxFloat << endl;
+     // Floating-point Input
+     cout << "\nEnter two real numbers: ";
+     cin >> x >> y;
+     double maxFloat = max(x, y);
+     cout << "The maximum of " << x << " and " << y << " is: " << maxFloat << endl;
 
-    cout << "\n*************************************************************************" << endl;
-    cout << "                       End of Program 4th.                               " << endl;
-    cout << "*************************************************************************\n" << endl;
+     cout << "\n*************************************************************************\n";
+     cout << "                         End of Program 4                                 \n";
+     cout << "*************************************************************************\n\n";
 }
-
-//-----------------------------------------------------------------
 // Q5. Function Overloading to Add Different Data Types
-int Add(int a, int b)
+int sum(int a, int b)
 {
-    return a + b;
+     return a + b;
 }
-float Add(float a, float b)
+float sum(float a, float b)
 {
-    return a + b;
+     return a + b;
 }
-double Add(int a, double b)
+double sum(int a, double b)
 {
-    return a + b;
+     return a + b;
 }
-double Add(double a, int b)
+double sum(double a, int b)
 {
-    return a + b;
-}
-void printAddFunction()
-{
-    cout << endl << "*************************************************************************" << endl;
-    cout << endl << " Q5. Write function using function overloading to add two numbers having";
-    cout << endl << "     different data types";
-    cout << endl << "*************************************************************************" << endl << endl;
-
-    int a, b;
-    float x, y;
-    double p, q;
-
-    cout << "Enter two integers: ";
-    cin >> a >> b;
-    cout << "Sum (int + int): " << Add(a, b) << endl << endl;
-
-    cout << "Enter two floating-point numbers: ";
-    cin >> x >> y;
-    cout << "Sum (float + float): " << Add(x, y) << endl << endl;
-
-    cout << "Enter one integer and one double: ";
-    cin >> a >> p;
-    cout << "Sum (int + double): " << Add(a, p) << endl;
-
-    cout << "\n*************************************************************************" << endl;
-    cout << "                       End of Program 5th.                               " << endl;
-    cout << "*************************************************************************\n" << endl;
+     return a + b;
 }
 
-//-----------------------------------------------------------------
+void printSumFunction()
+{
+     cout << endl
+          << "*************************************************************************" << endl;
+     cout << endl
+          << " Q5. Write functions using function overloading to add two numbers having." << endl;
+     cout << "     different data types." << endl;
+     cout << "*************************************************************************" << endl
+          << endl;
+
+     int a, b;
+     float x, y;
+     double p;
+
+     cout << "Enter two integers: ";
+     cin >> a >> b;
+     cout << "Sum (int + int): " << sum(a, b) << endl
+          << endl;
+
+     cout << "Enter two floating-point numbers: ";1 2
+     cin >> x >> y;
+     cout << "Sum (float + float): " << sum(x, y) << endl
+          << endl;
+
+     cout << "Enter one integer and one double: ";
+     cin >> a >> p;
+     cout << "Sum (int + double): " << sum(a, p) << endl
+          << endl;
+
+     cout << "Enter one double and one integer: ";
+     cin >> p >> a;
+     cout << "Sum (double + int): " << sum(p, a) << endl;
+
+     cout << "\n*************************************************************************" << endl;
+     cout << "                       End of Program 5th.                               " << endl;
+     cout << "*************************************************************************\n"
+          << endl;
+}
 int main()
 {
-    // printSwap();
-    // printSumOf2or3Numbers();
-    // printAreaCalculations();
-    // printMaxOf2Numbers();
-     printAddFunction();
-    return 0;
+     // printSwapValues();
+     // printAdditionOf2or3Numbers();
+     // printAreaCalculations();
+     // printMaxOf2Numbers();
+     printSumFunction();
+     printf("\n");
+     return 0;
 }
